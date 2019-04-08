49b9296321c55872179d5e362ed7f0f32e9a0a57
vigorweijia <vigorweijia@gmail.com>
2019-04-08 22:44:49 +0800

    从多个文件读入有序的贡献者信息，新增了add和delete维度

--- FirstProj/picture.js
+++ FirstProj/picture.js
@@ -26,7 +26,7 @@ function getLinearScale(d) {
 
 
 //为设置折线点颜色的函数
-function getDotColor(d) {
+function getDotAColor(d) {
     color = "";
     if(d == 0) color = "#ffffff";
     else if(d > 0 && d <= 20) color = "#ffd0d0";
@@ -38,13 +38,17 @@ function getDotColor(d) {
     return color;
 }
 
+function getDotDColor(d) {
+    return "#0000ff";
+}
+
 padding = {top: 20, right: 20, bottom: 20, left: 20};
-rectStep = 10;
-rectWidth = 10;
+rectStep = 8;
+rectWidth = 8;
 rectHeight = 100;
 
 //画矩形及折线的函数
-function addRectAndCrvLine(dataset, dataline) {
+function addRectAndCrvLine(dataset, datalineA, datalineD) {
     var width = dataset.length * rectWidth + 50;
     var height = rectHeight;
     var svg = d3.select("body")
@@ -78,7 +82,7 @@ function addRectAndCrvLine(dataset, dataline) {
                 .transition()
                 .duration(800)//当鼠标放在矩形上时，矩形变成黄色
                 .attr("fill","#00f080");
-            var str = "commits: " + d + "\nmodify: " + dataline[i];
+            var str = "commits: " + d + "\nAdd: " + datalineA[i] + "\nDelete: " + datalineD[i];
             tooltip.html(str)     //设置数据显示框
                 .style("left", (d3.event.pageX - 20) + "px")
                 .style("top", (d3.event.pageY + 20) + "px")
@@ -98,35 +102,106 @@ function addRectAndCrvLine(dataset, dataline) {
         });
 
     //绘制折线图，和d3 v3版本不同
-    var oneLine = d3.line()
+    var oneLineA = d3.line()
         .x(function (d,i) {
-        return padding.left + 5 + i*rectStep;
+        return padding.left + 4 + i*rectStep;
         })
         .y(function (d,i) {
             return rectHeight - d;
         })
         .curve(d3.curveLinear);
     //需要注意把填充设置为none，然后设置描边宽度
-    svg.append("path").attr("class","line").attr("d",oneLine(dataline)).attr("fill","none").attr("stroke","#00ff00").attr("stroke-width","1.5px");
+    svg.append("path").attr("class","line").attr("d",oneLineA(datalineA)).attr("fill","none").attr("stroke","#008000").attr("stroke-width","1.5px");
 
-    var dot = svg.selectAll("circle")
-                 .data(dataline).enter()
+    var dotA = svg.selectAll("circle")
+                 .data(datalineA).enter()
                  .append("circle")
                  .attr("cx", function (d,i) {
-                    return padding.left + i*rectStep + 5;
+                    return padding.left + i*rectStep + 4;
                  })
                  .attr("cy",function (d,i) {
                     return rectHeight - d;
                  })
-                 .attr("r", 3)
+                 .attr("r", 2)
                  .attr("fill", function (d,i) {
-                    return getDotColor(d,i);
+                    return getDotAColor(d);
                  });
+
+    var oneLineD = d3.line()
+        .x(function (d,i) {
+            return padding.left + 4 + i*rectStep;
+        })
+        .y(function (d,i) {
+            return rectHeight - d;
+        })
+        .curve(d3.curveLinear);
+    //需要注意把填充设置为none，然后设置描边宽度
+    svg.append("path").attr("class","line").attr("d",oneLineD(datalineD)).attr("fill","none").attr("stroke","#f08000").attr("stroke-width","1.5px");
+
+    var dotD = svg.selectAll("polygon")
+        .data(datalineD).enter()
+        .append("polygon")
+        .attr("points", function (d,i) {
+            x1 = padding.left + i*rectStep + 2;
+            x2 = padding.left + i*rectStep + 6;
+            y1 = rectHeight - d + 2;
+            y2 = rectHeight - d + 2;
+            x3 = padding.left + i*rectStep + 4;
+            y3 = rectHeight - d - 2;
+            return x1 + "," + y1 + " " + x2 + "," + y2 + " " + x3 + "," + y3;
+        })
+        .attr("fill", function (d,i) {
+            return getDotDColor(d);
+        });
 }
 
 
 //下面调用
-for(i = 0; i < 10; i++) {
+function ReadFile(data) {
+    //var box = document.getElementById("hellonju");
+    var ret = [];
+    var str = data.trim().split("\n");
+    for(let i = 0; i < str.length; i++)
+        ret.push(str[i]);
+    //box.innerText = data;
+    return ret;
+}
+function FileInfo(filename) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        var s = ReadFile(xhr.responseText);
+        //console.log(s);
+        var dataset = [];
+        var datalineA = [];
+        var datalineD = [];
+        for(let i = 0; i < 200; i++) {
+            var information = s[i].trim().split(" ");
+            dataset.push(parseInt(information[1]));
+            datalineA.push(parseInt(information[2]));
+            datalineD.push(parseInt(information[3]));
+        }
+        addRectAndCrvLine(dataset,datalineA,datalineD);
+    };
+    try {
+        xhr.open("get", "data/"+filename, true);
+        xhr.send();
+    } catch (e) {
+        ReadFile(e.message);
+    }
+}
+
+FileInfo("p0.txt");
+FileInfo("p1.txt");
+FileInfo("p2.txt");
+FileInfo("p3.txt");
+FileInfo("p4.txt");
+FileInfo("p5.txt");
+FileInfo("p6.txt");
+FileInfo("p7.txt");
+FileInfo("p8.txt");
+FileInfo("p9.txt");
+
+/*for(i = 0; i < 10; i++) {
     dataset = [(i*67+3*i)%200];
     dataline = [(i*31+7*i)%100];
     for(j = 1; j < 100; j++) {
@@ -134,7 +209,7 @@ for(i = 0; i < 10; i++) {
         dataline.push((dataline[j-1]*dataline[j-1]+i*j+1)%100);
     }
     addRectAndCrvLine(dataset,dataline);
-}
+}*/
 
 
 
