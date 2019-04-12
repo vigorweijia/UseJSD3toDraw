947253a5b1accdb0b31ad24ad453a25057177f35
vigorweijia <vigorweijia@gmail.com>
2019-04-12 20:27:17 +0800

    变种图补充完成

--- FirstProj/SpectralVariant.js
+++ FirstProj/SpectralVariant.js
@@ -21,4 +21,62 @@ function getcolor(d) {
     else if(d > 85 && d <= 90) color = "#440000";
     else if(d > 90 && d <= 95) color = "#220000";
     else if(d > 95) color = "#000000";
+    return color;
 }
+rectWidth = 7;
+rectStep = 7;
+rectHeight = 100;
+left = 20;
+function addRect(dataset) {
+    //document.write(dataset.length);
+    var width = dataset.length * rectWidth + 50;
+    var height = rectHeight;
+    var svg = d3.select("body").append("svg").attr("height",height).attr("width",width);
+    var rect = svg.selectAll("rect").data(dataset).enter().append("rect")
+        .attr("datam",function (d) {
+            return d;
+        })
+        .attr("fill",function (d) {
+            console.log(d);
+            return getcolor(d);
+        })
+        .attr("x",function (d,i) {
+            return left + i*rectStep;
+        })
+        .attr("y",0)
+        .attr("width",rectWidth)
+        .attr("height",rectHeight);
+}
+
+function ReadFile(data) {
+    var ret = [];
+    var str = data.trim().split("\n");
+    for(let i = 0; i < str.length; i++) {
+        ret.push(str[i]);
+        //console.log(str[i]);
+    }
+    return ret;
+}
+
+function FileInfo(filename) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        var s = ReadFile(xhr.responseText);
+        var dataset = [];
+        for(let i = 0; i < 168; i++) {
+            dataset.push(parseInt(s[i]));
+            //console.log(s[i]);
+        }
+        addRect(dataset);
+    }
+    try {
+        xhr.open("get","data/"+filename,true);
+        xhr.send();
+    }catch (e) {
+        ReadFile(e.message);
+    }
+}
+
+FileInfo("d3_week_data.txt");
+FileInfo("rxjava_week_data.txt");
+FileInfo("tensorflow_week_data.txt");
